0d7f6109c3ecea1b15ce1ed50ccb7165fc6988fa
aaronmorris <ajmorris84@gmail.com>
2020-04-04 19:47:21 -0700

    Metadata Refresh

---
+++ .sfdx/typings/lwc/sobjects/Manager_Note__ChangeEvent.d.ts
@@ -0,0 +1,52 @@
+declare module "@salesforce/schema/Manager_Note__ChangeEvent.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/Manager_Note__ChangeEvent.ReplayId" {
+  const ReplayId:string;
+  export default ReplayId;
+}
+declare module "@salesforce/schema/Manager_Note__ChangeEvent.ChangeEventHeader" {
+  const ChangeEventHeader:any;
+  export default ChangeEventHeader;
+}
+declare module "@salesforce/schema/Manager_Note__ChangeEvent.Owner" {
+  const Owner:any;
+  export default Owner;
+}
+declare module "@salesforce/schema/Manager_Note__ChangeEvent.OwnerId" {
+  const OwnerId:any;
+  export default OwnerId;
+}
+declare module "@salesforce/schema/Manager_Note__ChangeEvent.Name" {
+  const Name:string;
+  export default Name;
+}
+declare module "@salesforce/schema/Manager_Note__ChangeEvent.CreatedDate" {
+  const CreatedDate:any;
+  export default CreatedDate;
+}
+declare module "@salesforce/schema/Manager_Note__ChangeEvent.CreatedBy" {
+  const CreatedBy:any;
+  export default CreatedBy;
+}
+declare module "@salesforce/schema/Manager_Note__ChangeEvent.CreatedById" {
+  const CreatedById:any;
+  export default CreatedById;
+}
+declare module "@salesforce/schema/Manager_Note__ChangeEvent.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/Manager_Note__ChangeEvent.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/Manager_Note__ChangeEvent.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/Manager_Note__ChangeEvent.Attachment_Links__c" {
+  const Attachment_Links__c:string;
+  export default Attachment_Links__c;
+}
---
+++ .sfdx/typings/lwc/sobjects/Private_Object__ChangeEvent.d.ts
@@ -0,0 +1,56 @@
+declare module "@salesforce/schema/Private_Object__ChangeEvent.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/Private_Object__ChangeEvent.ReplayId" {
+  const ReplayId:string;
+  export default ReplayId;
+}
+declare module "@salesforce/schema/Private_Object__ChangeEvent.ChangeEventHeader" {
+  const ChangeEventHeader:any;
+  export default ChangeEventHeader;
+}
+declare module "@salesforce/schema/Private_Object__ChangeEvent.Owner" {
+  const Owner:any;
+  export default Owner;
+}
+declare module "@salesforce/schema/Private_Object__ChangeEvent.OwnerId" {
+  const OwnerId:any;
+  export default OwnerId;
+}
+declare module "@salesforce/schema/Private_Object__ChangeEvent.Name" {
+  const Name:string;
+  export default Name;
+}
+declare module "@salesforce/schema/Private_Object__ChangeEvent.CreatedDate" {
+  const CreatedDate:any;
+  export default CreatedDate;
+}
+declare module "@salesforce/schema/Private_Object__ChangeEvent.CreatedBy" {
+  const CreatedBy:any;
+  export default CreatedBy;
+}
+declare module "@salesforce/schema/Private_Object__ChangeEvent.CreatedById" {
+  const CreatedById:any;
+  export default CreatedById;
+}
+declare module "@salesforce/schema/Private_Object__ChangeEvent.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/Private_Object__ChangeEvent.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/Private_Object__ChangeEvent.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/Private_Object__ChangeEvent.Account__c" {
+  const Account__c:any;
+  export default Account__c;
+}
+declare module "@salesforce/schema/Private_Object__ChangeEvent.notes__c" {
+  const notes__c:string;
+  export default notes__c;
+}
